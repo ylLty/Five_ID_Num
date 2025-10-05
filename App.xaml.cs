3cd569a9e2667afabc453b1b852bbb2f628eed77
yl_lty <wsc139qq@163.com>
2025-10-05 15:43:26 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Five_ID_Num
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
